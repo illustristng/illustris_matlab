f0413d17060b093ccaf031609121b94aa443ba45
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2015-03-20 18:24:47 -0400

    Group catalog loading.

---
+++ +illustris/+groupcat/gcPath.m
@@ -0,0 +1,10 @@
+function [filePath] = gcPath(basePath,snapNum,chunkNum)
+  % GCPATH  Return absolute path to a group catalog HDF5 file (modify as needed).
+  if ~exist('chunkNum','var')
+    chunkNum = 0;
+  end
+  
+  gcPath = [basePath '/groups_' num2str(snapNum,'%03d') '/'];
+  filePath = [gcPath 'groups_' num2str(snapNum,'%03d')];
+  filePath = [filePath '.' num2str(chunkNum) '.hdf5'];
+end

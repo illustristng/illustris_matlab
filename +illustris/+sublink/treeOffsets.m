a538d1e2403f347c301f33834bded3db11871404
Dylan Nelson <dnelson@cfa.harvard.edu>
2016-11-07 10:06:32 -0500

    Fix for loadMPB() with new scheme.

--- +illustris/+sublink/treeOffsets.m
+++ +illustris/+sublink/treeOffsets.m
@@ -10,22 +10,21 @@ function [off1,off2,off3] = treeOffsets(basePath,snapNum,id,treeName,prefix,offF
     filePath = groupcat.offsetPath(basePath,snapNum);
     [field_names, shapes, types] = hdf5_dset_properties(filePath, 'FileOffsets');
     groupFileOffsets = h5read(filePath, ['/FileOffsets/Subhalo'], 1, shapes.('Subhalo'));
+
+    offsetFile = groupcat.offsetPath(basePath,snapNum);
+    prefix = ['/Subhalo/' treeName '/'];
+
+    groupOffset = id;
   else
     % load groupcat chunk offsets from header of first file
     header = groupcat.loadHeader(basePath,snapNum);
     groupFileOffsets = header.('FileOffsets_Subhalo');
-  end
 
-  % calculate target groups file chunk which contains this id
-  groupFileOffsets = int64(id) - groupFileOffsets;
-  fileNum = max(find( groupFileOffsets >= 0 ));
-  groupOffset = double(groupFileOffsets(fileNum) + 1); % double for h5read
-  
-  % load the merger tree offsets of this subgroup
-  if strfind(groupcat.gcPath(basePath,snapNum),'fof_subhalo')
-    offsetFile = groupcat.offsetPath(basePath,snapNum);
-    prefix = ['/Subhalo/' treeName '/'];
-  else
+    % calculate target groups file chunk which contains this id
+    groupFileOffsets = int64(id) - groupFileOffsets;
+    fileNum = max(find( groupFileOffsets >= 0 ));
+    groupOffset = double(groupFileOffsets(fileNum) + 1); % double for h5read
+
     offsetFile = groupcat.gcPath(basePath,snapNum,fileNum-1);
     % prefix from input
   end

dc47de6d9421fbf18359926f9bed279d705a17b4
Dylan Nelson <dnelson@cfa.harvard.edu>
2017-02-14 10:20:53 -0500

    Fix tree loading for new offset scheme, and various minor (all working for 2016a). Fixes #1, #2 and #3.

--- +illustris/+groupcat/offsetPath.m
+++ +illustris/+groupcat/offsetPath.m
@@ -3,5 +3,4 @@
 function [filePath] = offsetPath(basePath,snapNum)
   % OFFSETPATH  Return absolute path to a separate offset file (modify as needed).
   filePath = [basePath '../postprocessing/offsets/offsets_' num2str(snapNum,'%03d') '.hdf5'];
-  filePath = strjoin(filePath,'');
 end
--- +illustris/+sublink/treePath.m
+++ +illustris/+sublink/treePath.m
@@ -7,11 +7,9 @@ function [filePath] = treePath(basePath,treeName,chunkNum)
   end
   
   filePath = [basePath '/trees/' treeName '/tree_extended.' num2str(chunkNum) '.hdf5'];
-  filePath = strjoin(filePath,'');
 
   if ~exist(strrep(filePath,'*','0'),'file')
     % new path scheme
     filePath = [basePath '/../postprocessing/trees/' treeName '/tree_extended.' num2str(chunkNum) '.hdf5'];
-    filePath = strjoin(filePath,'');
   end
 end

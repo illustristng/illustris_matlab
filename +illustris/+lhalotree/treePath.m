ffd383488bfa3a4d7ff67a51ebac7b4c70901657
Enrico Garaldi <EGaraldi@users.noreply.github.com>
2022-12-05 15:10:06 +0100

    makes treePath.m compatible with Thesan

--- +illustris/+lhalotree/treePath.m
+++ +illustris/+lhalotree/treePath.m
@@ -6,10 +6,16 @@ function [filePath] = treePath(basePath,snapNum,chunkNum)
     chunkNum = 0;
   end
   
-  filePath = [basePath '/trees/treedata/' 'trees_sf1_135.' num2str(chunkNum) '.hdf5'];
+  filePaths = {basePath + '/trees/treedata/' + 'trees_sf1_135.' + num2str(chunkNum) + '.hdf5';
+               basePath + '/../postprocessing/trees/LHaloTree/trees_sf1_099.' + num2str(chunkNum) + '.hdf5';
+               basePath + '/../postprocessing/trees/LHaloTree/trees_sf1_080.' + num2str(chunkNum) + '.hdf5'
+              };
 
-  if ~exist(strrep(filePath,'*','0'),'file')
-    % new path scheme
-    filePath = [basePath '/../postprocessing/trees/LHaloTree/trees_sf1_099.' num2str(chunkNum) '.hdf5'];
+  for k = 1:length(filePaths)
+    if exist(strcat(filePaths{k}),'file')
+        % new path scheme
+        filePath = filePaths{k};
+    end
   end
+
 end
